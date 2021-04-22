e76ca4e2bb80f2bc27f2bf91164134eaf8d8d4cb
Izuku152e <hassanhanif123429@gmail.com>
2021-04-22 09:13:18 +0500

    Switch b/w Forms

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,16 +1,29 @@
 <template>
     <div class="welcome container">
         <p>Welcome</p>
-        <!-- <SignUpForm /> -->
-        <LoginForm />
+        <SignUpForm v-if="showSignup" />
+        <LoginForm v-else />
+        <p v-if="!showSignup">
+            Don't have an account?
+            <span @click="showSignup = !showSignup">Sign Up</span>
+        </p>
+        <p v-else>
+            Already have an account?
+            <span @click="showSignup = !showSignup">Sign In</span>
+        </p>
     </div>
 </template>
 
 <script>
 import SignUpForm from "../components/SignupForm";
 import LoginForm from "../components/LoginForm";
+import { ref } from "vue";
 export default {
     components: { SignUpForm, LoginForm },
+    setup() {
+        const showSignup = ref(false);
+        return { showSignup };
+    },
 };
 </script>
 
@@ -43,5 +56,13 @@ export default {
 }
 .welcome button {
     margin: 20px auto;
+}
+.welcome span {
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+}
+.welcome button {
+    margin: 20px 0;
 }
 </style>
